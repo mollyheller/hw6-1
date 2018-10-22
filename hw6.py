e08ce0e0b55c9fd5b11233bbdbafc36403c7ff62
mollyheller <44239651+mollyheller@users.noreply.github.com>
2018-10-22 19:02:02 -0400

    updated homework

--- hw6.py
+++ hw6.py
@@ -2,14 +2,27 @@ import re
 import unittest
 
 def sumNums(fileName):
-    pass
+    data= open(fileName, 'r')
+    data= data.read()
+    nums = re.findall("(?<!\d|-)\d+(?!\d|-)", data)
+    total=0
+    for x in nums:
+        total += int(x)
+
+    return total
 
 def countWord(fileName, word):
-    pass
+    data= open(fileName, 'r')
+    data= data.read()
+    word_list = re.findall('(?i)' + str(word) + '(?!\w+)', data)
+
+    return len(word_list)
 
 def listURLs(fileName):
-    pass
-
+    data = open(fileName, 'r')
+    data = data.read()
+    urls = re.findall("\S+\.{1}\S+\.{1}\w+", data)
+    return urls
 
 class TestHW6(unittest.TestCase):
     """ Class to test this homework """
@@ -23,12 +36,11 @@ class TestHW6(unittest.TestCase):
         self.assertEqual(sumNums("regex_sum_132198.txt"), 374566)
 
     def test_countWord(self):
-        """ test count word on the first file """
-        self.assertEqual(countWord("regex_sum_42.txt", "computer"),21)
+       """ test count word on the first file """
+       self.assertEqual(countWord("regex_sum_42.txt", "computer"),21)
 
     def test_listURLs(self):
         """ test list URLs on the first file """
         self.assertEqual(len(listURLs("regex_sum_42.txt")), 3)
-
 # run the tests
 unittest.main(verbosity=2)
